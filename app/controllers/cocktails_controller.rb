d103f1312e3c604fe2b47848380d2181e7a16536
Daan Roelofs <daanroelofs599@gmail.com>
2020-08-20 15:29:28 +0200

    Added cocktails routes, controller and view

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,37 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+  end
+end
+
+private
+
+def cocktail_params
+    params.require(:cocktail).permit(:name, :doses, :ingredients)
+  end
+end
+
+
+# A user can see the list of cocktails
+# GET "cocktails"
+# A user can see the details of a given cocktail, with the dose needed for each ingredient
+# GET "cocktails/42"
+# A user can create a new cocktail
+# GET "cocktails/new"
+# POST "cocktails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Cocktail.destroy_all
+
+puts 'Creating cocktails...'
+whiskey_sour = { name: 'whiskey sour' }
+borreltje_cola = { name: 'borreltje cola' }
+long_island_ice_tea = { name: 'long island ice tea' }
+white_russian = { name: 'white russian' }
+basil_smash = { name: 'basil smash' }
+
+[whiskey_sour, borreltje_cola, long_island_ice_tea, white_russian, basil_smash].each do |attributes|
+  new_cocktail = Cocktail.create!(attributes)
+  puts "Created #{new_cocktail.name}"
+end
+puts 'Finished!'
