f62da4126ebc35de02a510c43b1b15cc7849e41a
Daan Roelofs <daanroelofs599@gmail.com>
2020-08-21 12:27:57 +0200

    added update mr. cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,7 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
   end
 
   def new
@@ -16,7 +17,7 @@ class CocktailsController < ApplicationController
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render 'new'
+      render :new
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
 puts 'Cleaning database...'
+Ingredient.destroy_all
 Cocktail.destroy_all
+puts 'Creating ingredients and cocktails...'
 
-puts 'Creating cocktails...'
-whiskey_sour = { name: 'whiskey sour' }
-borreltje_cola = { name: 'borreltje cola' }
-long_island_ice_tea = { name: 'long island ice tea' }
-white_russian = { name: 'white russian' }
-basil_smash = { name: 'basil smash' }
+lemon = Ingredient.create(name: 'lemon')
+ice = Ingredient.create(name: 'ice')
+mint_leaves = Ingredient.create(name: 'mint leaves')
 
-[whiskey_sour, borreltje_cola, long_island_ice_tea, white_russian, basil_smash].each do |attributes|
-  new_cocktail = Cocktail.create!(attributes)
-  puts "Created #{new_cocktail.name}"
+whiskey_sour = Cocktail.create(name: 'whiskey sour')
+white_russian = Cocktail.create(name: 'white russian')
+basil_smash = Cocktail.create(name: 'basil smash')
+
+x = Dose.create(description: 'Safari, Ice Tea, Vodka', cocktail_id: whiskey_sour.id, ingredient_id: mint_leaves.id)
+y = Dose.create(description: 'Vodka, Sprite, Redbull', cocktail_id: whiskey_sour.id, ingredient_id: lemon.id)
+z = Dose.create(description: 'Salt, Safari, IceTea', cocktail_id: whiskey_sour.id, ingredient_id: ice.id)
+[lemon, ice, mint_leaves].each do |ingredient|
+  puts "Created #{ingredient.name}"
+end
+[whiskey_sour, white_russian, basil_smash].each do |cocktail|
+  puts "Created #{cocktail.name}"
+end
+[x, y, z].each do |dose|
+  puts "Create #{dose.description}"
 end
 puts 'Finished!'
